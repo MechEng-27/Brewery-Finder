05efbfe9b9ab2777a726bf9cb4bf4ca4e9f56f2b
MechEng-27 <118615143+MechEng-27@users.noreply.github.com>
2023-03-13 19:17:41 -0500

    Added "about" section to main app page. Renders by default and also upon clicking "about" in nav-bar. Minor CSS tweaks.

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,11 +5,18 @@ import Card from "./Card";
 
 export default function Home() {
 
+    const [aboutSection, setAboutSection] = useState(true);
     const [error, setError] = useState(false);
     const [search, setSearch] = useState("");
     const [breweryResults, setBreweryResults] = useState([]);
 
     var currentYear = new Date().getFullYear();
+
+    const renderAbout = () => {
+        setAboutSection(true);
+        setError(false);
+        setBreweryResults([]);
+    }
     
     const breweryData = (input) => {
         fetch (`https://api.openbrewerydb.org/breweries?by_city=${input}`)
@@ -19,6 +26,7 @@ export default function Home() {
     
     const handleKeyDown = (event) => {
         if (event.key === "Enter"){
+            setAboutSection(false);
             breweryData(event.target.value);
             setSearch("");
             event.target.blur();
@@ -70,16 +78,45 @@ export default function Home() {
                     onClick = {clearSearch}>
                     </img>
                 </div>
-                <p id = "nav-bar__about">About</p>
+                <a id = "nav-bar__about" onClick = {renderAbout}>About</a>
             </nav>
             <div className = "home__content-container">
-                <div className = "home__content-cards">
-                {error ? <p>there is an error</p> : [breweryResults] }
-                </div>
-            </div>
-            <footer id = "footer">
-                <p>&#169; Brewery Finder {currentYear}</p>
-            </footer>
+                {aboutSection ?
+                <div id = "home__content-about">
+                    <h1>Welcome to Brewery Finder</h1>
+                    <p>
+                        The mission of Brewery Finder is to support breweries 
+                        of all sizes by allowing users to easily find what breweries 
+                        are local to them
+                    </p>
+                    <p>To use this application please enter your location 
+                        (city, town or village) into the search bar above and 
+                        hit enter!!!
+                    </p>
+                    <div id = "home__button-container">
+                        <p>Don't see your brewery listed?</p>
+                        <a href = "https://gentle-otter-5a7952.netlify.app/"
+                        target = "_blank">
+                        <button className = "home__button">
+                            Shoot us a note
+                            </button>
+                        </a>
+                    </div>
+                    <a id = "home__hyperlink" href = "https://www.openbrewerydb.org/">
+                        Powered by Open Brewery DB
+                    </a>
+                </div>: ""}
+                {error ?
+                <div id = "error__msg">
+                    <p>Unable to find a matching result</p>
+                    <p>Please check your entry and try again</p>
+                </div>: <div className = "home__content-cards">
+                    {[breweryResults]}
+                </div>}
         </div>
-    );
+        <footer id = "footer">
+            <p>&#169; Brewery Finder {currentYear}</p>
+        </footer>
+    </div>
+);
 };
